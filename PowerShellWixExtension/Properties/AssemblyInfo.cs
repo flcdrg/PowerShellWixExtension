ddbf29e8ba7af7e7a41ce3955bb6f0db016b17cd
Nir Bar <nir.bar@panel-sw.co.il>
2020-01-12 13:48:47 +0200

    Build "Any CPU" rather than x64

--- PowerShellWixExtension/Properties/AssemblyInfo.cs
+++ PowerShellWixExtension/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using Microsoft.Tools.WindowsInstallerXml;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 
 [assembly: AssemblyDefaultWixExtension(typeof(PowerShellWixExtension.PowerShellWixExtension))]
